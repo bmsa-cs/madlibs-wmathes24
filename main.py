742bb3b8da7d009457bbec2d0a40879c31f42d93
Wyatt Mathes <wmathes24@biomedscienceacademy.org>
2020-11-16 19:51:28 +0000

    I finished it

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: wyatt mathes 
+Period/Core: core 3
 
 
 """
+
+name=input("Enter your name")
+adjective=input("Enter an adjective")
+place=input("Enter a place")
+place_2=input("Enter a place")
+food=input("Enter an food")
+food_2=input("Enter another food")
+noun=input("Enter an noun")
+verb=input("Enter an verb")
+adverb=input("Enter an adverb")
+
+print(name + " was walking down the street when they real lized that they were in "+ place + ".") 
+print("Then "+ name +" started to walk to the local food market and then they picked up " + food +" and some "  + food_2 +"." )
+print("On "+ name + " walk home " + name +" had to "+ verb +" over a big hole in the road")
+print("when "+ name +"got back to "+ place +" they had to eat there " + food +" before it melted all over the place")
+print("Once "+ name +" was done eating " + food +"," + name +" ran in to a big group of "+ adjective +" that were very bad")
+print("By the " + adverb +" The people chaced them they were out of " + place +" and in " + place_2 +".")
